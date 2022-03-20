651861fbe208930f753ba02edc7485fd5a6b77a8
ali-alhaddad <alialhaddad708@gmail.com>
2022-03-20 17:27:36 +0300

    ali

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-alert("ali alhaddad");
-const style = "color:blue";
-console.log(4 * 4, style);
-console.log("انا في الصف العاشر");
-//[جافا سكربت ليست جافا]
+// alert("ali alhaddad");
+// const style = "color:blue";
+// console.log(4 * 4, style);
+// console.log("أنا في الصف العاشر ");
+// // // [جافاسكربت ليست جافا]
+
+// let budget = 20;
+// console.log(budget > 30 && budget < 80);
+// budget += 30;
+// console.log(budget > 30 && budget < 80);
+
+let grade = prompt("أدخل درجتك");
+// A = grade >= 90 && grade < 100;
+// S = grade >= 80;
+// D = grade >= 70;
+// F = grade >= 60;
+// H = grade >= 50;
+// G = grade <= 50;
+switch (grade) {
+  case "A":
+    console.log("لقد حصلت على امتياز 🥳");
+    break;
+  case "S":
+    console.log("لقد حصلت على جيد جداً🤩");
+    break;
+  case "D":
+    console.log("لقد حصلت على جيد🙂");
+    break;
+  case "F":
+    console.log("لقد حصلت على مقبول😕");
+    break;
+  case "H":
+    console.log("لقد حصلت على ضعيف☹️");
+    break;
+  case "G":
+    console.log("راسب");
+    break;
+  default:
+    console.log("دخلت رقم خاطئ");
+    break;
+}
